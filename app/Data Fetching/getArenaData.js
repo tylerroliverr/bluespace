36f0bc78ba67ff12cc8d65f538b1dac849b26bf7
tylerroliverr <tyler.oliverjames@gmail.com>
2024-04-29 13:54:55 +1000

    Removed landingHTML, removed useEffect

--- app/Data Fetching/getArenaData.js
+++ app/Data Fetching/getArenaData.js
@@ -1,105 +1,89 @@
 "use client";
-import { useEffect } from 'react';
+import { useState } from 'react';
+
+const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+const APIUrls = [
+    {
+        trinkets: "bluespace-trinkets-treasures",
+        etherial: "bluespace-etherial-religion",
+        patterns: "bluespace-patterns-shapes",
+        swords: "bluespace-swords-armour",
+        grass: "bluespace-grass-cottage",
+        graphic: "bluespace-graphic-digital"
+    }
+];
+
+const fetchData = async (type) => {
+    const apiUrl = `https://api.are.na/v2/channels/${type}/contents?per=100`;
+
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data.contents.filter(block => block.image && block.image.display && block.image.display.url && block.description).map(block => ({
+            imageUrl: block.image.display.url,
+            description: block.description,
+        }));
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+};
 
 export default function ArenaData() {
 
-    const shuffleArray = array => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    };
+    const [images, setImages] = useState([]);
 
-    const APIUrls = [
-        {
-            trinkets: "bluespace-trinkets-treasures",
-            etherial: "bluespace-etherial-religion",
-            patterns: "bluespace-patterns-shapes",
-            swords: "bluespace-swords-armour",
-            grass: "bluespace-grass-cottage",
-            graphic: "bluespace-graphic-digital"
-        }
-    ];
-
-    const fetchData = async (type) => {
-        const apiUrl = `https://api.are.na/v2/channels/${type}/contents?per=100`;
-    
+    const getImages = async (type) => {
         try {
-            const response = await fetch(apiUrl);
-    
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-    
-            const data = await response.json();
-
-            const filteredData = data.contents.filter(block => block.image && block.image.display && block.image.display.url && block.description);
-            
-            const mappedData = filteredData.map(block => ({
-                imageUrl: block.image.display.url,
-                description: block.description
-            }));
-
-            return mappedData;
-    
+            const fetchedImages = await fetchData(type);
+            const shuffledImages = shuffleArray(fetchedImages);
+            setImages(shuffledImages);
         } catch (error) {
             console.error('Error fetching data:', error);
-            return [];
         }
     };
 
-    useEffect(() => {
-        const getImages = async (type) => {
-            try {
-                const imageContainers = document.querySelector('.imageContainer');
-                imageContainers.innerHTML = ''; // Clear existing content
+    // Assuming you have some way to determine what the user clicked
+    const handleClick = (type) => {
+        getImages(type);
+    };
 
-                const images = await fetchData(type);
-                const shuffledImages = shuffleArray(images);
-
-                shuffledImages.forEach(({imageUrl, description}) => {
-                    const imageBox = document.createElement('div');
-                    imageBox.classList.add('imageBox');
-    
-                    const imgElement = document.createElement('img');
-                    imgElement.src = imageUrl;
-                    imgElement.alt = 'Image from API';
-                    imgElement.classList.add('imageElement');
-    
-                    const descriptionElement = document.createElement('p');
-                    descriptionElement.textContent = description;
-                    descriptionElement.classList.add('imageDescription');
-    
-                    imageBox.appendChild(imgElement);
-                    imageBox.appendChild(descriptionElement);
-                    imageContainers.appendChild(imageBox);
-                });
-
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        // Assuming you have some way to determine what the user clicked
-        const handleClick = (type) => {
-            getImages(type);
-        };
-
-        const trinketsButton = document.querySelector('#trinkets');
-        const swordsButton = document.querySelector('#swords');
-        const grassButton = document.querySelector('#grass');
-        const patternsButton = document.querySelector('#patterns');
-        const etherialButton = document.querySelector('#etherial');
-        const graphicButton = document.querySelector('#graphic');
-
-        trinketsButton.addEventListener('click', () => handleClick(APIUrls[0].trinkets));
-        swordsButton.addEventListener('click', () => handleClick(APIUrls[0].swords));
-        grassButton.addEventListener('click', () => handleClick(APIUrls[0].grass));
-        patternsButton.addEventListener('click', () => handleClick(APIUrls[0].patterns));
-        graphicButton.addEventListener('click', () => handleClick(APIUrls[0].graphic));
-        etherialButton.addEventListener('click', () => handleClick(APIUrls[0].etherial));
-
-    }, []);
+    return (
+        <>
+            <div className="wrapper">
+                <div className="leftMenu">
+                    <p onClick={() => handleClick(APIUrls[0].swords)} id="swords">swords</p>
+                    <p onClick={() => handleClick(APIUrls[0].trinkets)} id="trinkets">trinkets</p>
+                    <p onClick={() => handleClick(APIUrls[0].grass)} id="grass">grass</p>
+                    <p onClick={() => handleClick(APIUrls[0].patterns)} id="patterns">patterns</p>
+                    <p onClick={() => handleClick(APIUrls[0].etherial)} id="etherial">etherial</p>
+                    <p onClick={() => handleClick(APIUrls[0].graphic)} id="graphic">graphic</p>
+                </div>
+                <div className="imagesWrapper">
+                    <div className="imageContainer">
+                        {images.map(({ imageUrl, description }) => (
+                            <div key={imageUrl} className="imageBox">
+                                <img src={imageUrl} alt="Image from API" className="imageElement" />
+                                <p className="imageDescription">{description}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 
 }
--- app/Landing Page/landingPage.js
+++ app/Landing Page/landingPage.js
@@ -1,13 +1,11 @@
 import ArenaData from "../Data Fetching/getArenaData";
 import { Suspense } from "react";
-import LandingHTML from "./landingHTML";
 
 export default function LandingPage() {
     return (
         <>
             <Suspense>
-                <ArenaData />
-                <LandingHTML/>
+                <ArenaData/>
             </Suspense>
         </>
     )
